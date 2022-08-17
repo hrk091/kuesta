abca8f965373148f7607a087a9099fa2065e9909
Hiroki Okui <hiroki.okui@gmail.com>
2022-08-17 13:20:16 +0900

    test: Test for device compisite command and some test fixtures

--- pkg/nwctl/device_composite_test.go
+++ pkg/nwctl/device_composite_test.go
@@ -1,8 +1,12 @@
 package nwctl_test
 
 import (
+	"context"
+	"cuelang.org/go/cue/cuecontext"
 	"github.com/hrk091/nwctl/pkg/nwctl"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -48,4 +52,45 @@ func TestDeviceCompositeCfg_Validate(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestRunDeviceComposite(t *testing.T) {
+	want := []byte(`{
+	Interface: {
+		Ethernet1: {
+			Name:        "Ethernet1" @go(,*string)
+			Description: "foo"       @go(,*string)
+			Enabled:     true        @go(,*bool)
+			AdminStatus: 1
+			OperStatus:  1
+			Type:        80
+			Mtu:         9000 @go(,*uint16)
+			Subinterface: {} @go(,map[uint32]*Interface_Subinterface)
+		}
+		Ethernet2: {
+			Name:        "Ethernet2" @go(,*string)
+			Description: "bar"       @go(,*string)
+			Enabled:     false       @go(,*bool)
+			AdminStatus: 1
+			OperStatus:  1
+			Type:        80
+			Mtu:         9000 @go(,*uint16)
+			Subinterface: {} @go(,map[uint32]*Interface_Subinterface)
+		}
+	} @go(,map[string]*Interface)
+	Vlan: {} @go(,map[uint16]*Vlan)
+}
+`)
+	err := nwctl.RunDeviceComposite(context.Background(), &nwctl.DeviceCompositeCfg{
+		RootCfg: nwctl.RootCfg{RootPath: filepath.Join("./testdata")},
+		Device:  "oc01",
+	})
+	ExitOnErr(t, err)
+	got, err := os.ReadFile(filepath.Join("./testdata", "devices", "oc01", "config.cue"))
+	ExitOnErr(t, err)
+
+	cctx := cuecontext.New()
+	wantVal := cctx.CompileBytes(want)
+	gotVal := cctx.CompileBytes(got)
+	assert.True(t, wantVal.Equals(gotVal))
 }
