37ee666a4125352ebb5e3975b03872aafe413f02
Hiroki Okui <hiroki.okui@gmail.com>
2022-08-22 14:50:07 +0900

    refactor: Move git artifact utils to controller pkg from api pkg

--- provisioner/controllers/gitrepository_watcher.go
+++ provisioner/controllers/gitrepository_watcher.go
@@ -17,14 +17,18 @@ limitations under the License.
 package controllers
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha1"
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"github.com/fluxcd/pkg/untar"
 	"github.com/hrk091/nwctl/provisioner/api/v1alpha1"
 	"io"
 	"io/ioutil"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"net/http"
 	"os"
 	"path/filepath"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -66,7 +70,7 @@ func (r *GitRepositoryWatcher) Reconcile(ctx context.Context, req ctrl.Request) 
 	defer os.RemoveAll(tmpDir)
 
 	// download and extract artifact
-	summary, err := v1alpha1.FetchArtifact(ctx, repository, tmpDir)
+	summary, err := FetchArtifact(ctx, repository, tmpDir)
 	if err != nil {
 		l.Error(err, "unable to fetch artifact")
 		return ctrl.Result{}, err
@@ -135,4 +139,73 @@ func (r *GitRepositoryWatcher) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&sourcev1.GitRepository{}, builder.WithPredicates(GitRepositoryRevisionChangePredicate{})).
 		Complete(r)
+}
+
+func FetchArtifact(ctx context.Context, repository sourcev1.GitRepository, dir string) (string, error) {
+	if repository.Status.Artifact == nil {
+		return "", fmt.Errorf("respository %s does not containt an artifact", repository.Name)
+	}
+
+	url := repository.Status.Artifact.URL
+
+	// for local run:
+	// kubectl -n flux-system port-forward svc/source-controller 8080:80
+	// export SOURCE_HOST=localhost:8080
+	if hostname := os.Getenv("SOURCE_HOST"); hostname != "" {
+		url = fmt.Sprintf("http://%s/gitrepository/%s/%s/latest.tar.gz", hostname, repository.Namespace, repository.Name)
+	}
+
+	// download the tarball
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create HTTP request, error: %w", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil {
+		return "", fmt.Errorf("failed to download artifact from %s, error: %w", url, err)
+	}
+	defer resp.Body.Close()
+
+	// check response
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to download artifact, status: %s", resp.Status)
+	}
+
+	var buf bytes.Buffer
+
+	// verify checksum matches origin
+	if err := verifyArtifact(repository.GetArtifact(), &buf, resp.Body); err != nil {
+		return "", err
+	}
+
+	// extract
+	summary, err := untar.Untar(&buf, dir)
+	if err != nil {
+		return "", fmt.Errorf("faild to untar artifact, error: %w", err)
+	}
+
+	return summary, nil
+}
+
+func verifyArtifact(artifact *sourcev1.Artifact, buf *bytes.Buffer, reader io.Reader) error {
+	hasher := sha256.New()
+
+	// for backwards compatibility with source-controller v0.17.2 and older
+	if len(artifact.Checksum) == 40 {
+		hasher = sha1.New()
+	}
+
+	// compute checksum
+	mw := io.MultiWriter(hasher, buf)
+	if _, err := io.Copy(mw, reader); err != nil {
+		return err
+	}
+
+	if checksum := fmt.Sprintf("%x", hasher.Sum(nil)); checksum != artifact.Checksum {
+		return fmt.Errorf("failed to verify artifact: computed checksum '%s' doesn't match advertised '%s'",
+			checksum, artifact.Checksum)
+	}
+
+	return nil
 }
