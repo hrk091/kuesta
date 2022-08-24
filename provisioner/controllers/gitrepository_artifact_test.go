0940ed51233565ff3b7b50d48a4dd286e9f55c36
Hiroki Okui <hiroki.okui@gmail.com>
2022-08-24 11:21:38 +0900

    refactor: Move FetchArtifact to gitrepository_artifact

---
+++ provisioner/controllers/gitrepository_artifact_test.go
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2022. Hiroki Okui
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package controllers_test
+
+import (
+	"context"
+	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
+	"github.com/hrk091/nwctl/provisioner/controllers"
+	"github.com/stretchr/testify/assert"
+	"io"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestFetchArtifact(t *testing.T) {
+	dir := t.TempDir()
+	checksum, buf := mustGenTgzArchive("test.txt", "dummy")
+
+	tests := []struct {
+		name     string
+		handler  http.HandlerFunc
+		checksum string
+		wantErr  bool
+	}{
+		{
+			"ok",
+			func(w http.ResponseWriter, r *http.Request) {
+				if _, err := io.Copy(w, buf); err != nil {
+					panic(err)
+				}
+			},
+			checksum,
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := httptest.NewServer(tt.handler)
+			repo := sourcev1.GitRepository{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test-ns",
+				},
+				Status: sourcev1.GitRepositoryStatus{
+					Artifact: &sourcev1.Artifact{
+						URL:      h.URL,
+						Checksum: tt.checksum,
+					},
+				},
+			}
+			_, err := controllers.FetchArtifact(context.Background(), repo, dir)
+			assert.Nil(t, err)
+		})
+	}
+
+}
--- provisioner/controllers/gitrepository_watcher.go
+++ provisioner/controllers/gitrepository_watcher.go
@@ -17,18 +17,12 @@ limitations under the License.
 package controllers
 
 import (
-	"bytes"
 	"context"
-	"crypto/sha1"
-	"crypto/sha256"
 	"fmt"
-	"github.com/fluxcd/pkg/untar"
 	"github.com/hrk091/nwctl/pkg/nwctl"
 	"github.com/hrk091/nwctl/provisioner/api/v1alpha1"
-	"io"
 	"io/ioutil"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"net/http"
 	"os"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
@@ -114,73 +108,4 @@ func (r *GitRepositoryWatcher) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&sourcev1.GitRepository{}, builder.WithPredicates(GitRepositoryRevisionChangePredicate{})).
 		Complete(r)
-}
-
-func FetchArtifact(ctx context.Context, repository sourcev1.GitRepository, dir string) (string, error) {
-	if repository.Status.Artifact == nil {
-		return "", fmt.Errorf("respository %s does not containt an artifact", repository.Name)
-	}
-
-	url := repository.Status.Artifact.URL
-
-	// for local run:
-	// kubectl -n flux-system port-forward svc/source-controller 8080:80
-	// export SOURCE_HOST=localhost:8080
-	if hostname := os.Getenv(EnvSourceHost); hostname != "" {
-		url = fmt.Sprintf("http://%s/gitrepository/%s/%s/latest.tar.gz", hostname, repository.Namespace, repository.Name)
-	}
-
-	// download the tarball
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return "", fmt.Errorf("failed to create HTTP request, error: %w", err)
-	}
-
-	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
-	if err != nil {
-		return "", fmt.Errorf("failed to download artifact from %s, error: %w", url, err)
-	}
-	defer resp.Body.Close()
-
-	// check response
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("failed to download artifact, status: %s", resp.Status)
-	}
-
-	var buf bytes.Buffer
-
-	// verify checksum matches origin
-	if err := verifyArtifact(repository.GetArtifact(), &buf, resp.Body); err != nil {
-		return "", err
-	}
-
-	// extract
-	summary, err := untar.Untar(&buf, dir)
-	if err != nil {
-		return "", fmt.Errorf("failed to untar artifact, error: %w", err)
-	}
-
-	return summary, nil
-}
-
-func verifyArtifact(artifact *sourcev1.Artifact, buf *bytes.Buffer, reader io.Reader) error {
-	hasher := sha256.New()
-
-	// for backwards compatibility with source-controller v0.17.2 and older
-	if len(artifact.Checksum) == 40 {
-		hasher = sha1.New()
-	}
-
-	// compute checksum
-	mw := io.MultiWriter(hasher, buf)
-	if _, err := io.Copy(mw, reader); err != nil {
-		return err
-	}
-
-	if checksum := fmt.Sprintf("%x", hasher.Sum(nil)); checksum != artifact.Checksum {
-		return fmt.Errorf("failed to verify artifact: computed checksum '%s' doesn't match advertised '%s'",
-			checksum, artifact.Checksum)
-	}
-
-	return nil
 }
